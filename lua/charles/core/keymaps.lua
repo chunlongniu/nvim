084af155c5a370e13e68a8d60ef6850ad0fd9a5d
Charles Niu <chunlongniu@gmail.com>
2025-08-13 09:58:00 +0800

    Add hotkey for terminal and window management

--- lua/charles/core/keymaps.lua
+++ lua/charles/core/keymaps.lua
@@ -20,3 +20,25 @@ keymap.set("n",  "<leader>tn", "<cmd>tabn<CR>", {desc= "Go to next tab"})
 keymap.set("n",  "<leader>tp", "<cmd>tabp<CR>", {desc= "Go to previous tab"})
 keymap.set("n",  "<leader>tf", "<cmd>tabnew %<CR>", {desc= "Open current buffer in new tab"})
 
+-- ***********************window panel management ***********************
+-- pane management resize
+keymap.set('n', '<C-Up>', ':resize +2<CR>')
+keymap.set('n', '<C-Down>', ':resize -2<CR>')
+keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
+keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+-- panel maxsize
+keymap.set('n', '<leader>mo', '<C-w>o')
+keymap.set('n', '<leader>mh', '<C-w>_')
+keymap.set('n', '<leader>mw', '<C-w>|')
+keymap.set('n', '<leader>me', '<C-w>=')
+
+-- ***********************end ***********************
+
+-- bufferline
+keymap.set('n', '<Tab>', '<cmd>BufferLineCycleNext<CR>', { silent = true })
+keymap.set('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', { silent = true })
+
+-- terminal
+-- ctrl+ \ then ctrl+ n back to nornamal
+keymap.set('n', '<leader>th', '<cmd>split | terminal<CR>')
+keymap.set('n', '<leader>tv', '<cmd>vsplit | terminal<CR>')
--- lua/charles/plugins/bufferline.lua
+++ lua/charles/plugins/bufferline.lua
@@ -1,11 +1,18 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+   "akinsho/bufferline.nvim",
+   dependencies = { "nvim-tree/nvim-web-devicons" },
+   version = "*",
+   opts = {
+     options = {
+        mode = "buffers",
+        numbers = "ordinal",
+        diagnostics = "coc",
+        show_close_icon = true,
+        show_buffer_icons = true,
+        separator_style = "slant",
+        indicator = {
+            style = "full",
+        }
+     },
+   },
 }
